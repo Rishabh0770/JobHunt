8add150052e116899520a23b07926d712240a12f
Rishabh <rishabhverma0484@gmail.com>
2025-07-25 20:39:33 +0530

    header change
    added hamburger for mobile use

--- script.js
+++ script.js
@@ -173,3 +173,13 @@ function closeMessages() {
 
 
 
+
+
+// Hamburger Toggle
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('open');
+  navLinks.classList.toggle('active');
+});
